8da81e9ac0bc144b737a6ac8dcb61001d46fef47
Devone Austin <devone.austin@gmail.com>
2020-04-28 23:04:30 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,8 @@
 class BankAccount
-  attr_accessor :balance
+  attr_accessor :balance, :status
   attr_reader :name
 
-  def initialize name, balance= 1000
+  def initialize name, balance= 1000, status= "open"
     @name = name
     @balance = 1000
   end
