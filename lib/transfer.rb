1be32e3fee0cb283b425393f495d77fb88f6a25b
Devone Austin <devone.austin@gmail.com>
2020-04-28 23:57:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,10 @@
 class Transfer
-
-  def initialize
-  end
-
+  attr_accessor :amount
+  attr_reader :sender, :receiver
+  def initialize sender, receiver, amount
+      @sender = sender
+      @receiver = receiver
+      @status = "pending"
+      @amount = amount
+    end
 end
