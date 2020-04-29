e46267c7f719a802f2bfa8da96aac7d5e7ff7888
Devone Austin <devone.austin@gmail.com>
2020-04-29 00:40:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid?
+    if valid? && sender.balance > amount && self.status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
