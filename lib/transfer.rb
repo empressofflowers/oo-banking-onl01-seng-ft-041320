47ea47fced2fc72b1be5731859c1d47ce8a649e8
Devone Austin <devone.austin@gmail.com>
2020-04-29 00:11:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,6 +12,10 @@ class Transfer
   def valid?
     @sender.valid? && @receiver.valid?
   end
-
   
+  def execute_transaction
+    if valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+  end
 end
